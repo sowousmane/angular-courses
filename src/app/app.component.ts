294ff979138fc1b1273ef0752447a4ea7433725d
sowousmane <sowousmane4811@gmail.com>
2021-09-07 17:07:37 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemons';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'first-course';
+export class AppComponent implements OnInit {
+  title = 'List of pokemons';
+  value = 1;
+
+  //pokemons = new Pokemon();
+  pokemons: Pokemon[] = POKEMONS ;
+
+  ngOnInit(): void {}
+
+  onClick() {
+    console.log(this.pokemons);
+  }
 }
---
+++ src/app/pokemon.ts
@@ -0,0 +1,9 @@
+export class Pokemon {
+  id: number | undefined;
+  hp: number | undefined;
+  cp: number | undefined;
+  name: string | undefined;
+  picture: string | undefined;
+  types: Array<string> | undefined;
+  created: Date | undefined;
+}
